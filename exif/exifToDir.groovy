55bb9e652c8fca325b5fba6dbc8255dd154dd03e
Christian LIPP <Christian.Lipp@corba.at>
2018-01-23 16:29:22 +0100

    Supported files without EXIF header

--- exif/exifToDir.groovy
+++ exif/exifToDir.groovy
@@ -1,33 +1,68 @@
-import com.google.common.io.Files
-
 @Grapes([
     @Grab(group='com.drewnoakes', module='metadata-extractor', version='2.6.2')
 ])
 
-import static groovy.io.FileType.FILES
-
 import com.drew.imaging.ImageMetadataReader
 import com.drew.metadata.exif.ExifSubIFDDirectory
+import static groovy.io.FileType.FILES
 
-final String BASE_DIR = 'D:\\Fotos'
+class ExifMover
+{
+	final String BASE_DIR = '/Users/Christian/Pictures/Christians iPhone'
+	final boolean isProduction = true
 
-new File(BASE_DIR).eachFile(FILES) { file ->
+	void move()
+	{
+		new File(BASE_DIR).eachFile(FILES) { file ->
+			if (file.toString().toUpperCase().endsWith('.JPG')) {
 
-    // Datum des Fotos aus EXIF Daten einlesen
-    String date = ImageMetadataReader
-            .readMetadata(file)
-            .getDirectory(ExifSubIFDDirectory.class)
-            .getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)
-            .format('yyyyMMdd')
+				// Datum des Fotos aus EXIF Daten einlesen
+				String date = null
+				try {
+					date = ImageMetadataReader
+						.readMetadata(file)
+						.getDirectory(ExifSubIFDDirectory.class)
+						.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)
+						.format('yyyyMM')
+//						.format('yyyyMMdd')
+				}
+				catch (Exception e) {
+					return
+				}
 
-    // Verzeichnis erzeugen, wenn es noch nicht existiert
-    def dir = new File("$BASE_DIR\\$date")
-    if (!dir.exists())
-    {
-        dir.mkdir()
-    }
+				// Verzeichnis erzeugen, wenn es noch nicht existiert
+				def dir = new File("${BASE_DIR}${File.separator}${date}")
+				makeDirectory(dir)
 
-    // Foto in das Verzeichnis verschieben
-    def destination = new File("$dir.absolutePath\\$file.name")
-    Files.move(file, destination)
+				// Foto in das Verzeichnis verschieben
+				def destination = new File("${dir.absolutePath}${File.separator}${file.name}")
+				moveFile(file, destination)
+			}
+		}
+	}
+
+	void makeDirectory(File directory)
+	{
+		if (isProduction) {
+			if (!directory.exists()) {
+				directory.mkdir()
+			}
+		}
+		else {
+			println "Make directory ${directory.path}"
+		}
+	}
+
+	void moveFile(File source, File destination)
+	{
+		if (isProduction) {
+			java.nio.file.Files.move(source.toPath(), destination.toPath())
+		}
+		else {
+			println "Move file from ${source.path} to ${destination.path}"
+		}
+	}
 }
+
+def mover = new ExifMover()
+mover.move()
